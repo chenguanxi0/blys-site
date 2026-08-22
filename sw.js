67f6a47a8851ce10a82b32c3d5c41ef2a0995a12
JamesZed <491788533@qq.com>
2026-08-22 09:35:48 +0800

    更新聊天室缓存版本

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'blys-v22';
+const CACHE_NAME = 'blys-v23';
 const ASSETS = [
   '/',
   '/index.html',
-  '/chat.html?v=2026082202',
+  '/chat.html?v=2026082203',
   '/daily.html',
   '/diary.html?v=2026082102',
   '/changelog.html',
   '/notification-guide.html?v=2026082105',
   '/assets/styles.css?v=2026082114',
-  '/assets/app.js?v=2026082202',
+  '/assets/app.js?v=2026082203',
   '/assets/favicon.png'
 ];
 
